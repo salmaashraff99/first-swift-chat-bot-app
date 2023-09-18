316ed868af4dcf62511f1245c8c3413672fa0b60
ahmedhany-188 <ahmedhany00_k.w.k@outlook.sa>
2023-09-18 12:22:28 +0300

    Initial Commit

---
+++ swift chat bot app/swift_chat_bot_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_chat_bot_appApp.swift
+//  swift chat bot app
+//
+//  Created by Ahmed Hany on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_chat_bot_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
